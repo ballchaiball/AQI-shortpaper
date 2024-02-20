bf0631c3b2c190d64cc479c5dfcbbbd563bb40b3
Nuttapat Pothavichai <nuttapatp@outlook.co.th>
2024-02-21 01:00:38 +0700

    debug favicon

--- src/main/java/com/api/des/controller/TestController.java
+++ src/main/java/com/api/des/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.api.des.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class TestController {
@@ -9,5 +11,11 @@ public class TestController {
     @GetMapping("/")
     public String test() {
         return "test";
+    }
+
+
+    @GetMapping("/favicon.ico")
+    public void favicon() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
